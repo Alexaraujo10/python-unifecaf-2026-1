cc6e34e87f9c56ae6391cfad6175ba781b0db406
Alex Machado <136132781+Alexaraujo10@users.noreply.github.com>
2026-04-02 22:22:46 -0300

    .

--- REx/e02_senha_certa.py
+++
@@ -1,5 +0,0 @@
-senha = str(input("Digite a senha:"))
-while senha == "unifecaf":
-    print("senha, incorreta, tente novamente")
-    senha = str(input("Digite a senha:"))
-    print("senha correta, acesso permitido") 
